f1d961b43f8eb53b8b26aeb47c6d4efc96dd2fc1
pervaiz <m.akhtar@hotmail.co.uk>
2025-05-05 19:40:15 +0100

    Add project files.

---
+++ DeepSeek_Coder_V2_Ollama_SemanticKernel/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SemanticKernel;
+
+#pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+var kernelBuilder = Kernel.CreateBuilder()
+    .AddOllamaTextGeneration(
+       modelId: "deepseek-coder-v2:16b",
+       endpoint: new Uri("http://localhost:11434")
+    );
+#pragma warning restore SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+var kernel = kernelBuilder.Build();
+
+Console.WriteLine("Enter a description for the code generation:");
+var description = Console.ReadLine();
+
+var codeGenerationFunction = kernel.CreateFunctionFromPrompt(
+    $"{description}"
+);
+
+// Execute the function
+Console.WriteLine("Generating code...");
+var result = await kernel.InvokeAsync(codeGenerationFunction,
+    new() { { "description", description } }
+);
+
+// Print the result
+Console.WriteLine(result.GetValue<string>());
+
+
